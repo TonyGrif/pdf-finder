3d1ddd098170632fea21377f2074cc496c144bc0
TonyGrif <TonyGriffin2000@gmail.com>
2023-01-27 18:13:10 -0500

    added another test case

--- tests/test_pdf.py
+++ tests/test_pdf.py
@@ -7,6 +7,10 @@ class TestPDF:
     def test_bytes(self):
         testURI = "http://www.cs.odu.edu/~mln/pubs/ht-2018/hypertext-2018-nwala-bootstrapping.pdf"
         response = request(testURI)
-        
         result = PDF(response.headers['content-length'])
         assert int(result.bytes) == 994153
+        
+        testURI = "http://www.cs.odu.edu/~mln/pubs/ipres-2018/ipres-2018-atkins-news-similarity.pdf"
+        response = request(testURI)
+        result = PDF(response.headers['content-length'])
+        assert int(result.bytes) == 18995885
