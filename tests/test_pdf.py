4f30f0afadd324067990f6ef49d2b73ddd2231be
TonyGrif <TonyGriffin2000@gmail.com>
2023-12-15 20:18:54 -0500

    utilize pytest fixtures to boost testing performance

--- tests/test_pdf.py
+++ tests/test_pdf.py
@@ -4,51 +4,48 @@ from pdf import PdfFile
 from funcs import request
 
 
+@pytest.fixture
+def pdf():
+    test_uri = (
+        "http://www.cs.odu.edu/~mln/pubs/ht-2018/hypertext-2018-nwala-bootstrapping.pdf"
+    )
+    response = request(test_uri)
+    return PdfFile(response.headers["content-length"], test_uri, response.url)
+
+
 class TestPDF:
-    def test_bytes(self):
-        test_uri = "http://www.cs.odu.edu/~mln/pubs/ht-2018/hypertext-2018-nwala-bootstrapping.pdf"
-        response = request(test_uri)
-        result = PdfFile(response.headers["content-length"], test_uri, response.url)
-        assert int(result.bytes) == 994153
+    def test_bytes(self, pdf):
+        assert int(pdf.bytes) == 994153
 
         test_uri = "http://www.cs.odu.edu/~mln/pubs/ipres-2018/ipres-2018-atkins-news-similarity.pdf"
         response = request(test_uri)
         result = PdfFile(response.headers["content-length"], test_uri, response.url)
         assert int(result.bytes) == 18995885
 
-    def test_startURL(self):
-        test_uri = "http://www.cs.odu.edu/~mln/pubs/ht-2018/hypertext-2018-nwala-bootstrapping.pdf"
-        response = request(test_uri)
-        result = PdfFile(response.headers["content-length"], test_uri, response.url)
+    def test_startURL(self, pdf):
         # Notice http instead of https
         assert (
-            result.start_url
+            pdf.start_url
             == "http://www.cs.odu.edu/~mln/pubs/ht-2018/hypertext-2018-nwala-bootstrapping.pdf"
         )
         assert (
-            result.start_url
+            pdf.start_url
             != "https://www.cs.odu.edu/~mln/pubs/ht-2018/hypertext-2018-nwala-bootstrapping.pdf"
         )
 
-    def test_finalURI(self):
-        test_uri = "http://www.cs.odu.edu/~mln/pubs/ht-2018/hypertext-2018-nwala-bootstrapping.pdf"
-        response = request(test_uri)
-        result = PdfFile(response.headers["content-length"], test_uri, response.url)
+    def test_finalURI(self, pdf):
         # Notice https instead of http
         assert (
-            result.final_uri
+            pdf.final_uri
             == "https://www.cs.odu.edu/~mln/pubs/ht-2018/hypertext-2018-nwala-bootstrapping.pdf"
         )
         assert (
-            result.final_uri
+            pdf.final_uri
             != "http://www.cs.odu.edu/~mln/pubs/ht-2018/hypertext-2018-nwala-bootstrapping.pdf"
         )
 
-    def test_str(self):
-        test_uri = "http://www.cs.odu.edu/~mln/pubs/ht-2018/hypertext-2018-nwala-bootstrapping.pdf"
-        response = request(test_uri)
-        result = PdfFile(response.headers["content-length"], test_uri, response.url)
-        str_result = str(result.__str__)
+    def test_str(self, pdf):
+        str_result = str(pdf.__str__)
 
         assert str_result.find("994153")
         assert str_result.find(
